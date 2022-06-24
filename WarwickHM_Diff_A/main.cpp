a10996a611f6b58cb1971defc6b20bab6fa91904
Warwick <WarwickSeven@gmail.com>
2022-06-24 12:35:49 +0300

    Clear Main

--- WarwickHM_Diff_A/main.cpp
+++ WarwickHM_Diff_A/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  WarwickHM_Diff_A
-//
-//  Created by Александр Тихонов on 24.06.2022.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
